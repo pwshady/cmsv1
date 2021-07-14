05e3d7c351b1df8ac3ce21fbda1447eb99d155ff
pwshady <pwshady@gmail.com>
2021-07-14 09:27:08 +0300

    start

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <title>Добро пожаловать!</title>
+</head>
+
+<body>
+    <h1>test</h1>
+</body>
+
+</html>
