a877c4815c16402e68ef1b0ac156bbfb14f665b6
pwshady <pwshady@gmail.com>
2021-07-14 20:14:13 +0300

    14/07/2021

--- index.php
+++ index.php
@@ -8,11 +8,77 @@
 <head>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <link href="public/style/style.css" rel="stylesheet">
     <title>Добро пожаловать!</title>
 </head>
 
-<body>
-    <h1>test</h1>
+<body class="guest__page">
+    <header class="guest__header">
+        <div class="guest__header__left">
+            <nav class="guest__header__nav__left">
+                <ul>
+                    <li class="guest__header__nav__left__left">
+                        <!--Menu-->
+                        <div id="menuToggle">
+                        <input type="checkbox" />
+                        <span id="span1"></span>
+                        <span id="span2"></span>
+                        <span id="span3"></span>
+                    </div>
+                    </li>
+                    <li class="guest__header__nav__left__center">
+                        2
+                    </li>
+                    <li class="guest__header__nav__left__right">
+                        3
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="guest__header__center">
+            <nav class="guest__header__nav__center">
+                <ul>
+                    <li>
+                        1
+                    </li>
+                    <li>
+                        2
+                    </li>
+                    <li>
+                        3
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="guest__header__right">
+            <nav class="guest__header__nav__right">
+                <ul>
+                    <li class="guest__header__nav__right__left">
+                        1
+                    </li>
+                    <li class="guest__header__nav__right__center">
+                        2
+                    </li>
+                    <li class="guest__header__nav__right__right">
+                        3
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <section class="guest__content">
+        <div class="guest__content_left">
+            a
+        </div>
+        <div class="guest__content_center">
+        Prepared do an dissuade be so whatever steepest. Yet her beyond looked either day wished nay. By doubtful disposed do juvenile an. Now curiosity you explained immediate why behaviour. An dispatched impossible of of melancholy favourable. Our quiet not heart along scale sense timed. Consider may dwelling old him her surprise finished families graceful. Gave led past poor met fine was new. 
+        </div>
+        <div class="guest__content_right">
+            c
+        </div>
+    </section>
 </body>
 
 </html>
